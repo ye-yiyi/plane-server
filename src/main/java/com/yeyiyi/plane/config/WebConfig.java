6a9c2fcaf4c580b376c588d5499c9b4ecb152e70
guo.mingyi <guo.mingyi@ustcinfo.com>
2024-03-22 09:49:20 +0800

    11

---
+++ src/main/java/com/yeyiyi/plane/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.yeyiyi.plane.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author YeYiYi
+ * @date 2024/3/21 15:15
+ * @description
+ */
+//@Configuration
+//public class WebConfig {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+//}
